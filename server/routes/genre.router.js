836f169323901e78e700eb30b50a072ba7c38cff
Paul LeRoy <Pleroy33@gmail.com>
2024-01-29 07:17:03 -0600

    fixed genre router

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool')
 router.get('/:id', (req, res) => {
   console.log('inside router')
   const query =`
-  SELECT movies.id, movies.title, movies.poster, movies.description, string_agg(genres.name, ', ') AS genres FROM "movies"
+  SELECT movies.id, movies.title, movies.poster, movies.description FROM "movies"
   JOIN "movies_genres" ON movies.id=movies_genres.movie_id
   JOIN "genres" ON movies_genres.genre_id=genres.id
   WHERE movies.id=$1
